53a46f105ed95de95325959aed55bc41873f2b91
KohYungChunIvan <s10262491@connect.np.edu.sg>
2023-11-28 23:00:06 +0800

    Added Shopping Cart and rename cake images

--- java script.js
+++ java script.js
@@ -88,7 +88,6 @@ function resetForm() {
     document.getElementById('file').value = '';
 }
 
-// Optional: You can handle form submission using AJAX or any other method you prefer
 document.getElementById('customForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -100,4 +99,131 @@ document.getElementById('customForm').addEventListener('submit', function (event
     resetForm();
 });
 
+// Cart functionality
+// Cart array initialization
+var cart = [];
 
+// Function to add a cake to the cart
+function addToCart(cakeName, clickedElement) {
+    if (!cart) {
+        // If 'cart' is not properly initialized, initialize it
+        cart = [];
+    }
+
+    // Get the price from the price tag inside the clicked element
+    const priceTag = clickedElement.parentElement.parentElement.querySelector('.price-tag');
+    const price = parseFloat(priceTag.textContent.replace('$', ''));
+
+    cart.push({ name: cakeName, price: price });
+    updateCartCount();
+    updateCartPopupContent();
+
+    // You can add other actions if needed
+    console.log('Cake added to cart:', cakeName, 'Price:', price);
+}
+
+function updateCartCount() {
+    const cartCount = document.getElementById('cart-count');
+    cartCount.textContent = cart.length;
+}
+
+
+function updateCartPopupContent() {
+    const cartPopupContent = document.querySelector('.cart-popup-content');
+    const emptyCartMessage = document.querySelector('.empty-cart-title');
+
+    // Clear existing content
+    cartPopupContent.innerHTML = '';
+
+    if (cart.length === 0) {
+        // If cart is empty, show the empty cart message
+        emptyCartMessage.style.display = 'block';
+    } else {
+        // If cart has items, hide the empty cart message
+        emptyCartMessage.style.display = 'none';
+
+        cart.forEach((cake, index) => {
+            // Create a cart item container
+            const cartItem = document.createElement('div');
+            cartItem.className = 'cart-item';
+            cartItem.style.display = 'flex'; // Set display to flex
+            cartItem.style.alignItems = 'center'; // Center items vertically
+            cartItem.style.marginBottom = '15px'; // You can adjust the spacing as needed
+            cartItem.style.paddingBlockEnd = '30px';
+
+            // Create a delete icon
+            const deleteIcon = document.createElement('span');
+            deleteIcon.innerHTML = '&minus;'; // This is the "x" symbol
+            deleteIcon.className = 'delete-icon';
+            deleteIcon.setAttribute('data-index', index); // Save the index for deletion
+            deleteIcon.addEventListener('click', deleteCakeFromCart);
+
+            // Create an image element for the cake
+            const cakeImage = document.createElement('img');
+            cakeImage.src = `photos/${cake.name}.jpg`; // Use cake.name instead of cakeName
+            cakeImage.className = 'cart-cake-image';
+
+            // Create a div to hold cake name and price
+            const infoContainer = document.createElement('div');
+            infoContainer.style.marginLeft = '13px'; // Adjust margin as needed
+
+            // Create a span for the cake name
+            const cakeNameSpan = document.createElement('span');
+            cakeNameSpan.textContent = cake.name;
+
+            // Create a span for the price
+            const priceSpan = document.createElement('span');
+            priceSpan.textContent = `$${cake.price.toFixed(2)}`; // Display the price with 2 decimal places
+
+            // Append the delete icon, cake image, and info container to the cart item
+            cartItem.appendChild(deleteIcon);
+            cartItem.appendChild(cakeImage);
+            cartItem.appendChild(infoContainer);
+
+            // Append the cake name span to the info container
+            infoContainer.appendChild(cakeNameSpan);
+
+            // Append the price span to the info container on a new line
+            infoContainer.appendChild(document.createElement('br')); // Add line break
+            infoContainer.appendChild(priceSpan);
+
+            // Append the cart item to the cart popup content
+            cartPopupContent.appendChild(cartItem);
+        });
+    }
+}
+
+function deleteCakeFromCart(event) {
+    const index = event.target.getAttribute('data-index');
+    
+    // Remove the cake from the cart based on the index
+    cart.splice(index, 1);
+
+    // Update the cart count and cart popup content
+    updateCartCount();
+    updateCartPopupContent();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all elements with class 'plus-icon'
+    const plusIcons = document.querySelectorAll('.plus-icon');
+
+    // Add click event listener to each plus icon
+    plusIcons.forEach(function(icon) {
+        icon.addEventListener('click', function() {
+            // Get the cake name associated with the clicked plus icon
+            const cakeName = this.getAttribute('data-cake-name');
+            // Get the price from the data-price attribute
+            const price = parseFloat(this.getAttribute('data-price')); // Parse the price as a float
+
+            // Add the cake to the cart
+            addToCart(cakeName, price);
+        });
+    });
+});
+
+
+function toggleCart() {
+    const cartPopup = document.querySelector('.cart-popup');
+    cartPopup.style.right = cartPopup.style.right === '0px' ? '-500px' : '0px';
+}
